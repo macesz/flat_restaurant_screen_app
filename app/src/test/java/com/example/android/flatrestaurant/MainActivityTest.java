eabb3151ede186c5b022bff8648a1464c791c5b6
macesz <szabo.orsolya.levele@gmail.com>
2017-04-05 12:55:38 +0300

    first commit

---
+++ app/src/test/java/com/example/android/flatrestaurant/MainActivityTest.java
@@ -0,0 +1,16 @@
+package com.example.android.flatrestaurant;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by orsi on 08/02/2017.
+ */
+public class MainActivityTest {
+    @Test
+    public void onCreate() throws Exception {
+
+    }
+
+}
